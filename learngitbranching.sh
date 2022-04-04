b74a6a747771f489cf8bee025165ac7c63ba0317
MichalKuc <dsw46651@student.dsw.edu.pl>
2022-04-04 12:51:00 -0500

    Zadanie wykonane

---
+++ learngitbranching.sh
@@ -0,0 +1,17 @@
+git commit
+git commit
+
+git checkout -b bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
